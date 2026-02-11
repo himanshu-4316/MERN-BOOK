846162a495c963131268db7f8ce6a8e440b0a0b3
CHM04 <chmcs2026.himanshu.bhoir@gmail.com>
2026-02-11 09:37:18 +0530

    first commit

---
+++ backend/src/server.js
@@ -0,0 +1,30 @@
+
+import express from "express";
+import dotenv from "dotenv";
+import bookRoutes from"./routes/bookRoutes.js"
+import { connectDB } from "./config/db.js"
+import cors  from "cors";
+dotenv.config()
+const app=express();
+const port=process.env.port || 3000
+//app.get('/',(req,res)=>{
+    //res.status(200).json("Hello" )
+    //console.log('Hello')
+//})
+//app.listen(port,() =>{
+    //console.log(`http://localhost:${port}`)
+//})
+
+// console.log(bookRoutes)
+app.use(cors(
+    {
+        origin: 'http://localhost:5173'
+    }
+))
+app.use(express.json())
+app.use("/books", bookRoutes)
+connectDB().then(() =>{
+    app.listen(port, () =>{
+        console.log(`http://localhost:${port}/books`)
+    })
+})
