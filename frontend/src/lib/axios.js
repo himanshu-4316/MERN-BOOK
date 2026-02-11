5fd98168dbc92566c21d8685681d82b77bd13205
himanshu-4316 <chmcs2026.himanshu.bhoir@gmail.com>
2026-02-11 10:25:51 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 const api = axios.create({
-    baseURL: 'http://localhost:3000'
+    //baseURL: 'http://localhost:3000'
+    baseURL:'https://mern-book-73f8.onrender.com'
 })
 export default api
